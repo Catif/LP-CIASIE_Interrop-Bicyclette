2630d5aa057bd473ff136c2419c6e612ff59cd76
Catif <bradley.barbier@outlook.fr>
2023-02-26 16:13:37 +0100

    Update MeteoApi.php

--- assets/function/api/MeteoApi.php
+++ assets/function/api/MeteoApi.php
@@ -1,12 +1,13 @@
 <?php
 function getDataMeteo($city)
 {
+  $url = "https://www.prevision-meteo.ch/services/xml/" . $city;
+
   // Options pour le proxy Webetu
   // $opts = ['http' => ['proxy' => 'www-cache:3128', 'request_fulluri' => true], 'ssl' => ['verify_peer' => false, 'verify_peer_name' => false]];
   // $context = stream_context_create($opts);
-
-  $url = "https://www.prevision-meteo.ch/services/xml/" . $city;
   // $xml = file_get_contents($url, false, $context);
+
   $xml = file_get_contents($url, false);
   $data = simplexml_load_string($xml);
 
